1456d5c495fe2c2983c4c2e672111525e600c86a
heymarl <mumbao222@gmail.com>
2022-02-03 21:18:19 +0800

    design

---
+++ RNL Website/DESIGN/RNLVisionCare/contact.php
@@ -0,0 +1,323 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+-->
+<!doctype html>
+<html lang="zxx">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>RNL Vision Care | Contact
+	</title>
+	<!-- Template CSS -->
+	<link rel="stylesheet" href="assets/css/style-starter.css">
+	<!-- Template CSS -->
+	<link href="//fonts.googleapis.com/css?family=Poppins:300,400,400i,500,600,700&display=swap" rel="stylesheet">
+	<!-- Template CSS -->
+</head>
+
+<body>
+	
+	<!--w3l-header-->
+	<header class="w3l-header-nav">
+		<!--/nav-->
+		<nav class="navbar navbar-expand-lg navbar-light px-lg-0 py-0 px-3 stroke">
+			<div class="container">
+				<a class="navbar-brand" href="index.php"><span>RNL </span>Vision Care</a>
+				<!-- if logo is image enable this   
+						<a class="navbar-brand" href="#index.html">
+							<img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+						</a> -->
+				<button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
+					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="fa icon-expand fa-bars"></span>
+					<span class="fa icon-close fa-times"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mx-lg-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="index.php">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="about.php">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="services.php">Services</a>
+						</li>
+						
+						<li class="nav-item active">
+							<a class="nav-link" href="contact.php">Contact</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contact.php">Product</a>
+						</li>
+					</ul>
+					<!--/search-right-->
+					<div class="search-right">
+						<a href="#search" title="search"><span class="fa fa-search" aria-hidden="true"></span></a>
+						<!-- search popup -->
+						<div id="search" class="pop-overlay">
+							<div class="popup">
+
+								<form action="#" method="post" class="search-box">
+									<input type="search" placeholder="Saerch your Keyword" name="search"
+										required="required" autofocus="">
+									<button type="submit" class="btn"><span class="fa fa-search"
+											aria-hidden="true"></span></button>
+								</form>
+
+							</div>
+							<a class="close" href="#close">×</a>
+						</div>
+						<!-- /search popup -->
+					</div>
+					<!--//search-right-->
+					<div class="call-support">
+						<p>Call us for any question</p>
+						<h5>121-345-64369</h5>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<!--//nav-->
+	</header>
+	<!-- //w3l-header -->
+	<!-- /contact-form -->
+	<section class="w3l-contact-main">
+		<div class="contant11-top-bg py-5">
+			<div class="container py-md-5">
+				<div class="row contact-info-left text-center">
+					<div class="col-lg-4 col-md-6 contact-info">
+						<div class="contact-gd">
+							<span class="fa fa-location-arrow" aria-hidden="true"></span>
+							<h4>Address</h4>
+							<p>4885 Pretty View Lane, Lorem ipsum, Dolor sit amet, New York, USA</p>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 contact-info">
+						<div class="contact-gd">
+							<span class="fa fa-phone" aria-hidden="true"></span>
+							<h4>Phone</h4>
+							<p><a href="tel:+44 7834 857829">+44 7834 857829</a></p>
+							<p><a href="tel:+44 987 654 321">+44 987 654 321</a></p>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 contact-info">
+						<div class="contact-gd">
+							<span class="fa fa-envelope-open-o" aria-hidden="true"></span>
+							<h4>Mail</h4>
+							<p><a href="mailto:mail@example.com" class="email">mail@example.com</a></p>
+							<p><a href="mailto:mail@example.com" class="email">mail@example.com</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+	<!-- //contact-form -->
+	<!-- /contact-form -->
+	<section class="w3l-contact-main">
+		<div class="contact-infhny py-5">
+			<div class="container py-lg-5">
+				<div class="title-content text-left mb-lg-5 mt-4">
+					<h6 class="sub-title">Contact Us</h6>
+					<h3 class="hny-title">Drop your message for any info <br>or question.</h3>
+				</div>
+				<div class="row align-form-map">
+					<div class="col-lg-6 form-inner-cont">
+						<form action="https://sendmail.w3layouts.com/submitForm" method="post" class="signin-form">
+							<div class="form-input">
+								<label for="w3lName">Name*</label>
+								<input type="text" name="w3lName" id="w3lName" placeholder="" />
+							</div>
+							<div class="form-input">
+								<label for="w3lSender">Email*</label>
+								<input type="email" name="w3lSender" id="w3lSender" placeholder="" required="" />
+							</div>
+
+							<div class="form-input">
+								<label for="w3lMessage">Message*</label>
+								<textarea placeholder="" name="w3lMessage" id="w3lMessage" required=""></textarea>
+							</div>
+						
+							<button type="submit" class="btn btn-contact">Submit</button>
+
+						</form>
+					</div>
+					<div class="map col-lg-6 pl-lg-4">
+						<iframe
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.311966262144!2d121.0365460501551!3d14.75144597726703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b0386a31d5d1%3A0xc75f36c9f7afed54!2sCamarin%20Rd%2C%20Caloocan%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1643636272732!5m2!1sen!2sph"
+							frameborder="0" allowfullscreen=""></iframe>
+					</div>
+				</div>
+			</div>
+	</section>
+	<!-- //contact-form -->
+
+	<!-- footer-66 -->
+	<footer class="w3l-footer-66">
+		<!-- footer -->
+		<div class="footer-66-info">
+			<div class="container">
+				<div class="row footer-top">
+					<div class="col-lg-4 footer-grid_section_w3layouts pr-lg-5">
+						<h2 class="logo-2 mb-lg-4 mb-3">
+							<a class="navbar-brand" href="index.php"><span>Dent</span>ile</a>
+							<!-- if logo is image enable this   
+									<a class="navbar-brand" href="#index.html">
+										<img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+									</a> -->
+						</h2>
+						<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+							commodo consequat.</p>
+						<h4 class="sub-con-fo ad-info my-4">Catch on Social</h4>
+						<ul class="w3layouts_social_list list-unstyled">
+							<li>
+								<a href="#" class="w3pvt_facebook">
+									<span class="fa fa-facebook-f"></span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="w3pvt_twitter">
+									<span class="fa fa-twitter"></span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="w3pvt_dribble">
+									<span class="fa fa-dribbble"></span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="w3pvt_google">
+									<span class="fa fa-google-plus"></span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-8 footer-right">
+						<div class="w3layouts-news-letter">
+							<h3 class="footer-title mb-0">Newsletter</h3>
+
+							<p class="mb-3">By subscribing to our mailing list you will always get latest news and
+								updates from us.
+							</p>
+							<form action="#" method="post" class="w3layouts-newsletter">
+								<input type="email" name="Email" placeholder="Enter your email..." required="">
+								<button class="btn1"> Subscribe</button>
+
+							</form>
+						</div>
+						<div class="bottom-w3layouts-sec-nav">
+							<div class="row sub-content-botom mx-0">
+								<div class="col-md-4 footer-grid_section_w3layouts pl-lg-0">
+									<h3 class="footer-title">Information</h3>
+									<ul class="footer list-unstyled">
+										<li>
+											<a href="index.php">Home</a>
+										</li>
+										<li>
+											<a href="about.php">About Us</a>
+										</li>
+										<li>
+											<a href="blog.php">Blog</a>
+										</li>
+										<li>
+											<a href="services.php">Services</a>
+										</li>
+										<li>
+											<a href="contact.php">Contact Us</a>
+										</li>
+									</ul>
+								</div>
+								<div class="col-md-4 footer-grid_section_w3layouts">
+									<!-- social icons -->
+
+									<h3 class="footer-title">Treatments</h3>
+									<ul class="footer list-unstyled">
+
+										<li>
+											<a href="#">Preventative Dentistry</a>
+										</li>
+										<li>
+											<a href="#">Child Dentistry</a>
+										</li>
+										<li>
+											<a href="#">Affordable Dentures</a>
+										</li>
+										<li>
+											<a href="#">Tooth Extraction</a>
+										</li>
+
+									</ul>
+
+									<!-- social icons -->
+								</div>
+								<div class="col-md-4 footer-grid_section_w3layouts ">
+									<h3 class="footer-title">Working Hours</h3>
+									<ul class="wrk-schedule-block">
+										<li>Monday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Tuesday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Wednesday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Thursday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Friday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Sat day-Sunday<span class="schedule-time">Closed</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="cpy-right py-3">
+			<p class="text-center">© 2020 Dentile. All rights reserved
+			</p>
+		</div>
+		<!-- move top -->
+		<button onclick="topFunction()" id="movetop" title="Go to top">
+			<span class="fa fa-level-up"></span>
+		</button>
+		<script>
+			// When the user scrolls down 20px from the top of the document, show the button
+			window.onscroll = function () {
+				scrollFunction()
+			};
+
+			function scrollFunction() {
+				if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+					document.getElementById("movetop").style.display = "block";
+				} else {
+					document.getElementById("movetop").style.display = "none";
+				}
+			}
+
+			// When the user clicks on the button, scroll to the top of the document
+			function topFunction() {
+				document.body.scrollTop = 0;
+				document.documentElement.scrollTop = 0;
+			}
+		</script>
+		<!-- /move top -->
+	</footer>
+	<!--//footer-66 -->
+</body>
+
+</html>
+
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<!-- disable body scroll which navbar is in active -->
+
+<!--//-->
+<script>
+	$(function () {
+		$('.navbar-toggler').click(function () {
+			$('body').toggleClass('noscroll');
+		})
+	});
+</script>
+<!-- //script -->
+
+<script src="assets/js/bootstrap.min.js"></script>
---
+++ RNL Website/DESIGN/RNLVisionCare/services.php
@@ -0,0 +1,452 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+-->
+<!doctype html>
+<html lang="zxx">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>RNL Vision Care | Services
+	</title>
+	<!-- Template CSS -->
+	<link rel="stylesheet" href="assets/css/style-starter.css">
+	<!-- Template CSS -->
+	<link href="//fonts.googleapis.com/css?family=Poppins:300,400,400i,500,600,700&display=swap" rel="stylesheet">
+	<!-- Template CSS -->
+</head>
+
+<body>
+
+	<!--w3l-header-->
+	<header class="w3l-header-nav">
+		<!--/nav-->
+		<nav class="navbar navbar-expand-lg navbar-light px-lg-0 py-0 px-3 stroke">
+			<div class="container">
+				<a class="navbar-brand" href="index.php"><span>RNL </span>Vision Care</a>
+				<!-- if logo is image enable this   
+						<a class="navbar-brand" href="#index.html">
+							<img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+						</a> -->
+				<button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
+					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="fa icon-expand fa-bars"></span>
+					<span class="fa icon-close fa-times"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mx-lg-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="index.php">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="about.php">About</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="services.php">Services</a>
+						</li>
+						
+						<li class="nav-item">
+							<a class="nav-link" href="contact.php">Contact</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contact.php">Product</a>
+						</li>
+					</ul>
+					<!--/search-right-->
+					<div class="search-right">
+						<a href="#search" title="search"><span class="fa fa-search" aria-hidden="true"></span></a>
+						<!-- search popup -->
+						<div id="search" class="pop-overlay">
+							<div class="popup">
+
+								<form action="#" method="post" class="search-box">
+									<input type="search" placeholder="Saerch your Keyword" name="search"
+										required="required" autofocus="">
+									<button type="submit" class="btn"><span class="fa fa-search"
+											aria-hidden="true"></span></button>
+								</form>
+
+							</div>
+							<a class="close" href="#close">×</a>
+						</div>
+						<!-- /search popup -->
+					</div>
+					<!--//search-right-->
+					<div class="call-support">
+						<p>Call us for any question</p>
+						<h5>121-345-64369</h5>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<!--//nav-->
+	</header>
+	<!-- //w3l-header -->
+	<!-- /breadcrumbs -->
+	<nav id="breadcrumbs" class="breadcrumbs">
+		<div class="container page-wrapper">
+			<a href="index.php">Home</a> » <span class="breadcrumb_last" aria-current="page">Services</span>
+		</div>
+	</nav>
+	<!-- //breadcrumbs -->
+	<!-- services block 2 -->
+	<div class="w3l-services-block py-5" id="classes">
+		<div class="container py-lg-5 py-md-5">
+			<div class="title-content text-left mb-lg-5 mt-4">
+				<h6 class="sub-title">What We Offer</h6>
+				<h3 class="hny-title">Our Services</h3>
+				<p>Lorem illum facere aperiam consectetur adipisicing elit</p>
+			</div>
+			<div class="services-block_grids_bottom">
+				<div class="row">
+					<div class="col-lg-4 col-md-6 service_grid_btm_left mt-lg-5 mt-4">
+						<div class="service_grid_btm_left1">
+							<a href="#"><img src="assets/images/g1.jpg" alt=" " class="img-fluid" /></a>
+							<div class="service_grid_btm_left2">
+								<h5>Cosmetic Dentistry</h5>
+								<p>Maecenas sodales eu velit in varius. vitae sem vitae urna tempus commodo.</p>
+								<div class="read">
+									<a class="btn mt-4" href="services.php">Read More</a>
+								</div>
+							</div>
+
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 service_grid_btm_left mt-lg-5 mt-4">
+						<div class="service_grid_btm_left1">
+							<a href="#"><img src="assets/images/g2.jpg" alt=" " class="img-fluid" /></a>
+							<div class="service_grid_btm_left2">
+
+								<h5>Pediatric Dentistry</h5>
+								<p>Maecenas sodales eu velit in varius. vitae sem vitae urna tempus commodo.</p>
+								<div class="read">
+									<a class="btn mt-4" href="services.php">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 service_grid_btm_left mt-lg-5 mt-4">
+						<div class="service_grid_btm_left1">
+							<a href="#"><img src="assets/images/g3.jpg" alt=" " class="img-fluid" /></a>
+							<div class="service_grid_btm_left2">
+								<h5>Dental Implants</h5>
+								<p>Maecenas sodales eu velit in varius. vitae sem vitae urna tempus commodo.</p>
+								<div class="read">
+									<a class="btn mt-4" href="services.php">Read More</a>
+								</div>
+							</div>
+
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 service_grid_btm_left mt-lg-5 mt-4">
+						<div class="service_grid_btm_left1">
+							<a href="#"><img src="assets/images/g4.jpg" alt=" " class="img-fluid" /></a>
+							<div class="service_grid_btm_left2">
+								<h5>Preventive Dentistry</h5>
+								<p>Maecenas sodales eu velit in varius. vitae sem vitae urna tempus commodo.</p>
+								<div class="read">
+									<a class="btn mt-4" href="services.php">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 service_grid_btm_left mt-lg-5 mt-4">
+						<div class="service_grid_btm_left1">
+							<a href="#"><img src="assets/images/g5.jpg" alt=" " class="img-fluid" /></a>
+							<div class="service_grid_btm_left2">
+								<h5>Periodontal Therapy</h5>
+								<p>Maecenas sodales eu velit in varius. vitae sem vitae urna tempus commodo.</p>
+								<div class="read">
+									<a class="btn mt-4" href="services.php">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 service_grid_btm_left mt-lg-5 mt-4">
+						<div class="service_grid_btm_left1">
+							<a href="#"><img src="assets/images/g6.jpg" alt=" " class="img-fluid" /></a>
+							<div class="service_grid_btm_left2">
+								<h5>Teeth Whitening</h5>
+								<p>Maecenas sodales eu velit in varius. vitae sem vitae urna tempus commodo.</p>
+								<div class="read">
+									<a class="btn mt-4" href="services.php">Read More</a>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // services block2 -->
+	<section class="features-4">
+		<div class="features4-block py-5">
+			<div class="container py-lg-5">
+				<div class="title-content text-left mb-lg-5 mt-4">
+					<h6 class="sub-title-1">Features</h6>
+					<h3 class="hny-title two">Some More Features</h3>
+					<p class="fea-para">Lorem illum facere aperiam consectetur adipisicing elit</p>
+				</div>
+				<div class="row features4-grids text-left">
+					<div class="col-lg-3 col-md-6 features4-grid">
+						<div class="features4-grid-inn">
+							<span class="fa fa-pencil-square-o icon-fea4" aria-hidden="true"></span>
+							<h5><a href="#URL">Free Check up</a></h5>
+							<p>Lorem ipsum dolor sit amet,Ea consequuntur illum facere.</p>
+
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 features4-grid">
+						<div class="features4-grid-inn">
+							<span class="fa fa-umbrella icon-fea4" aria-hidden="true"></span>
+							<h5><a href="#URL">Always Open</a></h5>
+							<p>Lorem ipsum dolor sit amet,Ea consequuntur illum facere.</p>
+
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 features4-grid third">
+						<div class="features4-grid-inn">
+							<span class="fa fa-cogs icon-fea4" aria-hidden="true"></span>
+							<h5><a href="#URL">
+								Serve with Smile</a></h5>
+							<p>Lorem ipsum dolor sit amet,Ea consequuntur illum facere.</p>
+
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 features4-grid">
+						<div class="features4-grid-inn">
+							<span class="fa fa-spinner icon-fea4" aria-hidden="true"></span>
+							<h5><a href="#URL">Work with Hearts</a></h5>
+							<p>Lorem ipsum dolor sit amet,Ea consequuntur illum facere.</p>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	</section>
+	<!-- features-4 -->
+	<!-- /specification-6-->
+	<section class="w3l-specification-6">
+		<div class="specification-6-mian py-5">
+			<div class="container py-lg-5">
+				<div class="row story-6-grids text-left">
+					<div class="col-lg-5 story-gd">
+						<img src="assets/images/ab1.jpg" class="img-fluid" alt="/">
+					</div>
+					<div class="col-lg-7 story-gd pl-lg-4">
+						<div class="title-content text-left mb-lg-5 mt-4">
+							<h6 class="sub-title">For a Better Eyesight</h6>
+							<h3 class="hny-title">Caring Technologies For Your Eye Health</h3>
+							<p>Lorem illum facere aperiam sequi optio consectetur adipisicing elitFuga, suscipit totam
+								animi
+								consequatur saepe blanditiis.Lorem ipsum dolor sit amet</p>
+						</div>
+						<div class="skill_info mt-lg-5 mt-4">
+							<div class="stats_left">
+								<div class="counter_grid">
+									<div class="icon_info">
+										<p class="counter">165</p>
+										<h4>Daily appointments</h4>
+										<p class="counter-para">Lorem ipsum dolor sit amet consectetur adipisicing elit
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="stats_left">
+								<div class="counter_grid">
+									<div class="icon_info">
+										<p class="counter">563</p>
+										<h4>Total Clients</h4>
+										<p class="counter-para">Lorem ipsum dolor sit amet consectetur adipisicing elit
+										</p>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //specification-6-->
+
+	<!-- footer-66 -->
+	<footer class="w3l-footer-66">
+		<!-- footer -->
+		<div class="footer-66-info">
+			<div class="container">
+				<div class="row footer-top">
+					<div class="col-lg-4 footer-grid_section_w3layouts pr-lg-5">
+						<h2 class="logo-2 mb-lg-4 mb-3">
+							<a class="navbar-brand" href="index.php"><span>RNL </span>Vision Care</a>
+							<!-- if logo is image enable this   
+									<a class="navbar-brand" href="#index.html">
+										<img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+									</a> -->
+						</h2>
+						<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+							commodo consequat.</p>
+						<h4 class="sub-con-fo ad-info my-4">Catch on Social</h4>
+						<ul class="w3layouts_social_list list-unstyled">
+							<li>
+								<a href="#" class="w3pvt_facebook">
+									<span class="fa fa-facebook-f"></span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="w3pvt_twitter">
+									<span class="fa fa-twitter"></span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="w3pvt_dribble">
+									<span class="fa fa-dribbble"></span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="w3pvt_google">
+									<span class="fa fa-google-plus"></span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-8 footer-right">
+						<div class="w3layouts-news-letter">
+							<h3 class="footer-title mb-0">Newsletter</h3>
+
+							<p class="mb-3">By subscribing to our mailing list you will always get latest news and
+								updates from us.
+							</p>
+							<form action="#" method="post" class="w3layouts-newsletter">
+								<input type="email" name="Email" placeholder="Enter your email..." required="">
+								<button class="btn1"> Subscribe</button>
+
+							</form>
+						</div>
+						<div class="bottom-w3layouts-sec-nav">
+							<div class="row sub-content-botom mx-0">
+								<div class="col-md-4 footer-grid_section_w3layouts pl-lg-0">
+									<h3 class="footer-title">Information</h3>
+									<ul class="footer list-unstyled">
+										<li>
+											<a href="index.php">Home</a>
+										</li>
+										<li>
+											<a href="about.php">About Us</a>
+										</li>
+										<li>
+											<a href="blog.php">Blog</a>
+										</li>
+										<li>
+											<a href="services.php">Services</a>
+										</li>
+										<li>
+											<a href="contact.php">Contact Us</a>
+										</li>
+									</ul>
+								</div>
+								<div class="col-md-4 footer-grid_section_w3layouts">
+									<!-- social icons -->
+
+									<h3 class="footer-title">Treatments</h3>
+									<ul class="footer list-unstyled">
+
+										<li>
+											<a href="#">Preventative Dentistry</a>
+										</li>
+										<li>
+											<a href="#">Child Dentistry</a>
+										</li>
+										<li>
+											<a href="#">Affordable Dentures</a>
+										</li>
+										<li>
+											<a href="#">Tooth Extraction</a>
+										</li>
+
+									</ul>
+
+									<!-- social icons -->
+								</div>
+								<div class="col-md-4 footer-grid_section_w3layouts ">
+									<h3 class="footer-title">Working Hours</h3>
+									<ul class="wrk-schedule-block">
+										<li>Monday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Tuesday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Wednesday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Thursday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Friday<span class="schedule-time">09:00 – 17:00</span></li>
+										<li>Sat day-Sunday<span class="schedule-time">Closed</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="cpy-right py-3">
+			<p class="text-center">© 2020 Dentile. All rights reserved | Design by
+				<a href="http://w3layouts.com"> W3layouts.</a>
+			</p>
+		</div>
+		<!-- move top -->
+		<button onclick="topFunction()" id="movetop" title="Go to top">
+			<span class="fa fa-level-up"></span>
+		</button>
+		<script>
+			// When the user scrolls down 20px from the top of the document, show the button
+			window.onscroll = function () {
+				scrollFunction()
+			};
+
+			function scrollFunction() {
+				if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+					document.getElementById("movetop").style.display = "block";
+				} else {
+					document.getElementById("movetop").style.display = "none";
+				}
+			}
+
+			// When the user clicks on the button, scroll to the top of the document
+			function topFunction() {
+				document.body.scrollTop = 0;
+				document.documentElement.scrollTop = 0;
+			}
+		</script>
+		<!-- /move top -->
+	</footer>
+	<!--//footer-66 -->
+</body>
+
+</html>
+
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<!-- disable body scroll which navbar is in active -->
+
+<!--//-->
+<script>
+	$(function () {
+		$('.navbar-toggler').click(function () {
+			$('body').toggleClass('noscroll');
+		})
+	});
+</script>
+<!-- //script -->
+<!-- stats -->
+<script src="assets/js/jquery.waypoints.min.js"></script>
+<script src="assets/js/jquery.countup.js"></script>
+<script>
+	$('.counter').countUp();
+</script>
+<!-- //stats -->
+<script src="assets/js/bootstrap.min.js"></script>
