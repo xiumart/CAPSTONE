037f1fdbd016f497d4af85f67157d81b739a7119
heymarl <mumbao222@gmail.com>
2022-03-02 11:15:22 +0800

    admin login

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Admin Login | RNL Vision Care</title>
+<link rel="shorcut icon" type="img/png" href="images\logo.png">
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+</script>
+<!--webfonts-->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' rel='stylesheet' type='text/css'>
+<!--//webfonts-->
+</head>
+<body>
+
+<!--/start-login-one-->
+<h1></h1>
+		<div class="login">	
+			<div class="ribbon-wrapper h2 ribbon-red">
+				<div class="ribbon-front">
+					<h2>RNL Vision Care</h2>
+				</div>
+				<div class="ribbon-edge-topleft2"></div>
+				<div class="ribbon-edge-bottomleft"></div>
+			</div>
+			<form>
+				<ul>
+					<li>
+						<input type="text" class="text" value="Email Address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email Address';}" ><a href="#" class=" icon user"></a>
+					</li>
+					 <li>
+						<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}"><a href="#" class=" icon lock"></a>
+					</li>
+				</ul>
+
+			</form>
+			<div class="submit">
+				<input type="submit" onclick="myFunction()" value="Log in" >
+			</div>
+		</div>
+<!--start-copyright-->
+   		<div class="copy-right">
+				<p> &copy; 2020  All rights  Reserved | RNL Vision Care</a></p>
+		</div>
+	<!--//end-copyright-->
+</body>
+</html>
