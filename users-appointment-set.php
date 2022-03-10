1be2fc7d8981babaccdeae5843dd0ebf5c942745
jona <91324625+Pein1116@users.noreply.github.com>
2022-03-10 22:16:26 +0800

    update lang ng design

--- users-appointment-set.php
+++ users-appointment-set.php
@@ -5,6 +5,7 @@ session_start();
 <html lang="en" dir="ltr">
 
 <head>
+  <title>Appointments | RNL Vision Care</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <link rel="stylesheet" href="assets\css\user-style.css">
