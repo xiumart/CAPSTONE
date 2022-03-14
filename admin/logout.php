51675318aa76d40a3a5465f0f59e64b387903ee3
xiumart <94777387+xiumart@users.noreply.github.com>
2022-03-14 15:25:33 +0800

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -3,7 +3,7 @@ session_start();
 include "../conn.php";
 include "logs_conn.php";
 date_default_timezone_set('Asia/Manila');
-users_logs($_SESSION['users_roles'], "Logout", date("Y-m-d h:i:sa"), $_SESSION['users_roles']);
+users_logs($_SESSION['users_username'], "Logout", date("Y-m-d h:i:sa"), $_SESSION['users_roles']);
 session_unset();
 session_destroy();
 
