6370d5a5c007571b6a481956cd3e318cd312f19b
TREBOR0000 <john.robert.vitalicia@gmail.com>
2022-03-17 21:32:37 +0800

    maxlenght lang

--- users-appointment-set.php
+++ users-appointment-set.php
@@ -179,7 +179,7 @@ $user=$_SESSION['login_user'];
                             <input type="text" class="form-control" name="Name" id="w3lSubject"
                                 placeholder="Subject" required="">
                              <h3>Contact No.</h3>
-                            <input type="text" class="form-control" pattern="[0-9]{4}[0-9]{3}[0-9]{4}" name="Contact" id="w3lSubject"
+                            <input type="text" class="form-control" maxlength="11" pattern="[0-9]{4}[0-9]{3}[0-9]{4}" name="Contact" id="w3lSubject"
                                 placeholder="Subject" required="">
                                 <h3>Email</h3>
                             <input type="text" class="form-control" name="Email" id="w3lSubject"
