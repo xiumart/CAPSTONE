3f563fed35db0706557adac1843ce5b34d0afc8e
DanverOmlang <danver.omlang@digitizr.net.au>
2022-03-18 18:14:02 +0800

    SelectedAppointment

--- appointmenthandler.php
+++ appointmenthandler.php
@@ -19,7 +19,9 @@ $db = mysqli_connect("localhost", "root","", "capstone");
 		$purpose = $_POST['Purpose'];
 		$date = $_POST['Date'];
 		$time = $_POST['Time'];
-		$status = "PENDING";
+		$status = "PENDING"; 
+		$result = $db->query("SELECT * FROM appointment where app_time like '$time' and app_date like '$date'");
+		if ($result->num_rows == 0){
 
 		$sql = "INSERT INTO `appointment`(`app_user`, `app_name`, `app_email`, `app_contact`, `app_date`, `app_time`, `app_purpose`, `app_remarks`) VALUES ('$appuser','$name','$email','$contact','$date','$time', '$purpose', '$status')";
 		if(mysqli_query($db, $sql)){
@@ -28,13 +30,15 @@ $db = mysqli_connect("localhost", "root","", "capstone");
 	echo 'window.location="users-appointment.php";';
 	echo '</script>';
 	
-} else {
+}
+} 
+else {
 	echo '<script language="javascript">';
-	echo 'alert("Please Try Again");';
+	echo 'alert("Date and Time Has Taken, Please Select Again");';
 	echo 'window.location="users-appointment-set.php";';
 	echo '</script>';
 }
-?> 	
+
 
 
 
