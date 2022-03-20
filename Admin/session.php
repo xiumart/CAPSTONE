8b4dd6a7f7bea36059ea77f1302b4930aa49bd23
xiumart <94777387+xiumart@users.noreply.github.com>
2022-03-20 12:23:17 -0300

    Update session.php

--- Admin/session.php
+++ Admin/session.php
@@ -8,7 +8,7 @@
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
-   $login_session = $row['client_username'];
+   $login_session = $row['users_username'];
    
    if(!isset($_SESSION['login_user'])){
       header("location:index.php");
