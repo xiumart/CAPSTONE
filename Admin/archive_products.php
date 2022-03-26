d8e5cfa3c05748c507cc08f1912a329ca89f1b27
Danver John <80084411+DanverOmlang@users.noreply.github.com>
2022-03-26 12:41:58 +0800

    deletion

--- Admin/archive_products.php
+++ Admin/archive_products.php
@@ -1,28 +1,28 @@
 <?php 
-// creating a connection by passing server name, 
-// username, password and database name
-// servername=localhost
-// username=root
-// password=empty
-// database name= geeks_database
-$connection_link = new mysqli("localhost", "root", "","geeks_database"); 
+
+$connection_link = new mysqli("localhost", "root", "","capstone"); 
   
 if ($connection_link === false) { 
     die("ERROR: Not connected. ".$connection_link->connect_error); 
 } 
-  
+
+if (isset($_POST['btnrem'])) {
 //sql query to perform copying data from one table to another
-$sql_query  =  "insert table2 select * from table1 where "; 
+$pro_id1 = $_GET['id'];
+$sql_query  =  "insert archive_products select * from product where `pro_id` = $pro_id1 "; 
     if ($connection_link->query($sql_query) === true) 
 { 
-    echo "Data Copied Successfully."; 
+    echo '<script language="javascript">';
+	        echo 'alert("Products Move to Recycle Bin");';
+	        echo 'window.location="archive.php";';
+	        echo '</script>';
 } 
 else
 { 
     echo "ERROR: Could not able to proceed $sql_query. "
         .$connection_link->error; 
 } 
-  
+}
 // Close the  connection 
 $connection_link->close(); 
 ?>
